bc85939361da71cf8113207374bc42f70613ffd7
Falko Bräutigam <falko@polymap.de>
2016-04-13 23:24:24 +0200

    quietPeriod(0) - probieren was passiert

--- org.polymap.parent/jobs.groovy
+++ org.polymap.parent/jobs.groovy
@@ -232,7 +232,7 @@ repoProjects.each {
 
    mavenJob(jobName) {
      logRotator(-1, 5)
-     quietPeriod(3)
+     quietPeriod(0)
      blockOnUpstreamProjects()
 	   properties {
 	      githubProjectUrl('git@github.com:Polymap4/' + repoName + '.git')
